cf8638b019edc39a79c7ff7fde52d4eda0d96def
Edoardo Lolletti <edoardo762@gmail.com>
2024-10-26 16:15:42 +0200

    Don't change waitstate in gba booter dldi
    Allow supporting supercard lite with the slightly lower speeds, not really an issue since its purpose is to bootstrap a simple binary

--- SCFW_Stage2_GBA/source/new_scsdio.arm.cpp
+++ SCFW_Stage2_GBA/source/new_scsdio.arm.cpp
@@ -10,21 +10,9 @@ static bool isSDHC;
 uint8_t _SD_CRC7(uint8_t* pBuf, int len);
 bool SCSD_readData(void* buffer);
 
-// uint64_t sdio_crc16_4bit_checksum(uint32_t* data, uint32_t num_words);
-inline uint16_t setFastCNT(uint16_t originData) {
-	/*  2-3   32-pin GBA Slot ROM 1st Access Time (0-3 = 10, 8, 6, 18 cycles)
-		4     32-pin GBA Slot ROM 2nd Access Time (0-1 = 6, 4 cycles)*/
-	const uint16_t mask = ~(7 << 2);//~ 000011100, clear bit 2-3 + 4
-	const uint16_t setVal = ((2) << 2) | (1 << 4);
-	return (originData & mask) | setVal;
-}
-vu16* const EXMEMCNT_ADDR = (vu16*)0x4000204;
-
 void get_resp_drop(int dropBytes = 6);
 
 void ReadSector(uint8_t* buff, uint32_t sector, uint32_t readnum) {
-	u16 originMemStat = *EXMEMCNT_ADDR;
-	*EXMEMCNT_ADDR = setFastCNT(originMemStat);
 	auto param = isSDHC ? sector : (sector << 9);
 	if(readnum == 1) {
 		SDCommand(17, param);
@@ -40,7 +28,6 @@ void ReadSector(uint8_t* buff, uint32_t sector, uint32_t readnum) {
 	}
 
 	send_clk(0x10);       // Send clock signal
-	*EXMEMCNT_ADDR = originMemStat;
 }
 
 #define BUSY_WAIT_TIMEOUT 500000
