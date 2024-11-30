1d0070e2346f37cc9907d6ae1ba3025ed0f48d13
Edoardo Lolletti <edoardo762@gmail.com>
2024-11-30 16:14:19 +0100

    Update version

--- SCFW_Stage1/miniboot/source/arm9/console.c
+++ SCFW_Stage1/miniboot/source/arm9/console.c
@@ -99,7 +99,7 @@ void consoleInit(void) {
     {
         fontY = 1;
         fontPalette = 0x1000;
-        const char* str = "v1.1";
+        const char* str = "v1.2";
         fontX = (32 - 4) >> 1;
         eprintf("%s", str);
     }
