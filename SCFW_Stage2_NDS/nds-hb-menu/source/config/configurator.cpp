3ac37017741c621fdc70a68e1bb3acacb4cfa946
Edoardo Lolletti <edoardo762@gmail.com>
2024-12-14 17:51:59 +0100

    Add option to save/load configs from the sd
    Useful for when the firmware is updated

--- SCFW_Stage2_NDS/nds-hb-menu/source/config/configurator.cpp
+++ SCFW_Stage2_NDS/nds-hb-menu/source/config/configurator.cpp
@@ -6,6 +6,89 @@
 #include "../args.h"
 #include "../iconTitle.h"
 #include <nds.h>
+#include <sys/stat.h>
+
+char* getEntryPath(int off, struct SCSFW_CONFIGS* configs) {
+	switch(off) {
+		case 0:
+			return configs->hk_none.path;
+		case 1:
+			return configs->hk_a.path;
+		case 2:
+			return configs->hk_b.path;
+		case 3:
+			return configs->hk_x.path;
+		case 4:
+			return configs->hk_y.path;
+		default:
+			__builtin_unreachable();
+	}
+}
+
+bool readConfigsFromFile(struct SCSFW_CONFIGS* configs) {
+	struct SCSFW_CONFIGS tmpConfs{};
+	auto* file = fopen("fat:/scsfw/hotkeys.conf", "rb");
+	if(!file) {
+		iprintf("\x1b[2J");
+		iprintf("fat:/scsfw/hotkeys.conf\nnot found\n");
+		return false;
+	}
+	char line[sizeof(ENTRY::path) + 1]; // newline character would be included
+	for(int i = 0; i < 5; ++i) {
+		if(!fgets(line, sizeof(line), file)) {
+			iprintf("\x1b[2J");
+			iprintf("Invalid config file\n");
+			fclose(file);
+			return false;
+		}
+		auto len = strlen(line);
+		if(len > sizeof(ENTRY::path)) {
+			iprintf("\x1b[2J");
+			iprintf("Invalid config file\n");
+			fclose(file);
+			return false;
+		}
+		if(line[len-1] == '\n') {
+			line[len-1] = 0;
+			--len;
+		}
+		strcpy(getEntryPath(i, &tmpConfs), line);
+	}
+	fclose(file);
+	memcpy(configs, &tmpConfs, sizeof(struct SCSFW_CONFIGS));
+	return true;
+}
+
+bool dumpConfigsToFile(struct SCSFW_CONFIGS* configs) {
+	if(mkdir("fat:/scsfw", 0777) == -1 && errno != EEXIST) {
+		iprintf("\x1b[2J");
+		iprintf("failed to create fat:/scsfw folder\n");
+		return false;
+	}
+	auto* file = fopen("fat:/scsfw/hotkeys.conf", "wb");
+	if(!file) {
+		iprintf("\x1b[2J");
+		iprintf("failed to create fat:/scsfw/hotkeys.conf\n");
+		return false;
+	}
+	for(int i = 0; i < 5; ++i) {
+		auto* path = getEntryPath(i, configs);
+		fwrite(path, 1, strlen(path), file);
+		fwrite("\n", 1, 1, file);
+	}
+	fclose(file);
+	return true;
+}
+
+void printErrorAndWaitForA(const char* errorMessage) {
+	iprintf("%s\nPress A to continue", errorMessage);
+	while(1) {
+		scanKeys();
+		if(keysDownRepeat() & KEY_A)
+			return;
+		swiWaitForVBlank();
+	}
+}
 
 void configMenu(struct SCSFW_CONFIGS* configs) {
 	bool changed = false;
@@ -41,27 +124,14 @@ void configMenu(struct SCSFW_CONFIGS* configs) {
 		printEntry("X/UP   ", configs->hk_x);
 		printEntry("Y/LEFT ", configs->hk_y);
 		
-		auto getEntryPath = [&](int off) -> char* {
-			switch(off) {
-				case 0:
-					return configs->hk_none.path;
-				case 1:
-					return configs->hk_a.path;
-				case 2:
-					return configs->hk_b.path;
-				case 3:
-					return configs->hk_x.path;
-				case 4:
-					return configs->hk_y.path;
-				default:
-					__builtin_unreachable();
-			}
-		};
-		
+		auto restoreOffset = totalEntries;
+		printEntry("LOAD FROM FILE", restoreOffset);
+		auto dumpOffset = totalEntries;
+		printEntry("SAVE TO FILE", restoreOffset);
 		auto cancelOffset = totalEntries;
-		printEntry("CANCEL", cancelOffset);
+		printEntry("CANCEL", restoreOffset);
 		auto saveAndExitOffset = totalEntries;
-		printEntry("SAVE & EXIT", cancelOffset);
+		printEntry("SAVE & EXIT", restoreOffset);
 		
 		auto printCurEntry = [&]{
 			// move to row totalEntries + 2
@@ -73,12 +143,12 @@ void configMenu(struct SCSFW_CONFIGS* configs) {
 			
 			clearIconTitle();
 			
-			if(fileOffset != cancelOffset && fileOffset != saveAndExitOffset) {
-				auto* filename = getEntryPath(fileOffset);
+			if(fileOffset < restoreOffset) {
+				auto* filename = getEntryPath(fileOffset, configs);
 				iprintf(filename);
 				if(*filename != 0) {
 					iconTitleUpdate(false, filename);
-					iprintf("\x1b[%d;0H", totalEntries + 14);
+					iprintf("\x1b[%d;0H", totalEntries + 12);
 					iprintf("Press L or R to clear");
 				}
 			}
@@ -123,27 +193,33 @@ void configMenu(struct SCSFW_CONFIGS* configs) {
 					return;
 				} else if(fileOffset == saveAndExitOffset) {
 					goto end;
-				}
-				auto entry = browseForFile({".nds", ".srldr"});
-				std::string absPath;
-				toAbsPath(entry, nullptr, absPath);
-				if(absPath.size() >= sizeof(ENTRY::path)) {
-					iprintf("\x1b[2J");
-					iprintf("File path too long, press\n a to continue");
-					do {
-						scanKeys();
-						pressed = keysDownRepeat();
-						swiWaitForVBlank();
-					} while ((pressed & KEY_A) == 0);
+				} else if (fileOffset == restoreOffset) {
+					if(!readConfigsFromFile(configs)) {
+						printErrorAndWaitForA("Failed to read configs.");
+					} else {
+						changed = true;
+					}
+				} else if (fileOffset == dumpOffset) {
+					if(!dumpConfigsToFile(configs)) {
+						printErrorAndWaitForA("Failed to save configs.");
+					}
 				} else {
-					changed = true;
-					strcpy(getEntryPath(fileOffset), absPath.data());
+					auto entry = browseForFile({".nds", ".srldr"});
+					std::string absPath;
+					toAbsPath(entry, nullptr, absPath);
+					if(absPath.size() >= sizeof(ENTRY::path)) {
+						iprintf("\x1b[2J");
+						printErrorAndWaitForA("File path too long.");
+					} else {
+						changed = true;
+						strcpy(getEntryPath(fileOffset, configs), absPath.data());
+					}
 				}
 				break;
 			} else if(pressed & (KEY_L | KEY_R)) {
-				if(fileOffset != cancelOffset && fileOffset != saveAndExitOffset) {
+				if(fileOffset < restoreOffset) {
 					changed = true;
-					getEntryPath(fileOffset)[0] = 0;
+					getEntryPath(fileOffset, configs)[0] = 0;
 					break;
 				}
 			} else if (pressed & KEY_B) {
