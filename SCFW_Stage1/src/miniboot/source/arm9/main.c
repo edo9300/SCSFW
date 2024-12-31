c35a408bf52e8903698c0616b9a2a2c5a08dc1f0
Edoardo Lolletti <edoardo762@gmail.com>
2024-12-31 01:03:38 +0100

    Enable supercard's flash mode earlier in the bootloader

--- SCFW_Stage1/src/miniboot/source/arm9/main.c
+++ SCFW_Stage1/src/miniboot/source/arm9/main.c
@@ -47,42 +47,42 @@ void sysSetCartOwner(bool arm9) {
 #define SCSFW_MAGIC 0x57464353
 
 typedef struct SCSFW_PARAMETERS {
-	unsigned int scsfw_magic;
-	unsigned int miniboot_arm7;
-	unsigned int miniboot_arm7_size;
-	unsigned int miniboot_arm9;
-	unsigned int miniboot_arm9_size;
-	unsigned int nds_rom;
-	unsigned int nds_rom_size;
-	unsigned int sc_lite_dldi;
-	unsigned int sc_lite_dldi_size;
-	unsigned int scsd_dldi;
-	unsigned int scsd_dldi_size;
-	unsigned int sccf_dldi;
-	unsigned int sccf_dldi_size;
+    unsigned int scsfw_magic;
+    unsigned int miniboot_arm7;
+    unsigned int miniboot_arm7_size;
+    unsigned int miniboot_arm9;
+    unsigned int miniboot_arm9_size;
+    unsigned int nds_rom;
+    unsigned int nds_rom_size;
+    unsigned int sc_lite_dldi;
+    unsigned int sc_lite_dldi_size;
+    unsigned int scsd_dldi;
+    unsigned int scsd_dldi_size;
+    unsigned int sccf_dldi;
+    unsigned int sccf_dldi_size;
 } SCSFW_PARAMETERS;
 
 SCSFW_PARAMETERS parameters;
 
 void readNds(void* dest, unsigned int offset, unsigned int size) {
-	__aeabi_memcpy(dest, (void*)&GBA_BUS_U8[parameters.nds_rom + offset], size);
+    __aeabi_memcpy(dest, (void*)&GBA_BUS_U8[parameters.nds_rom + offset], size);
 }
 
 void findSCSFWParameters(SCSFW_PARAMETERS* params) {
-	__aeabi_memcpy4(params, (void*)&GBA_BUS_U8[0xc0 + 4], sizeof(SCSFW_PARAMETERS));
-	if(params->scsfw_magic == 0x57464353) {
-		dprintf("sclite magic found\n");
-		return;
-	}
-	// supercard rumble
-	__aeabi_memcpy4(params, (void*)&GBA_BUS_U8[0xc0 + 4 + 0x40000], sizeof(SCSFW_PARAMETERS));
-	// account for the values being offsetted
-	params->miniboot_arm7 += 0x40000;
-	params->miniboot_arm9 += 0x40000;
-	params->nds_rom += 0x40000;
-	params->sc_lite_dldi += 0x40000;
-	params->scsd_dldi += 0x40000;
-	params->sccf_dldi += 0x40000;
+    __aeabi_memcpy4(params, (void*)&GBA_BUS_U8[0xc0 + 4], sizeof(SCSFW_PARAMETERS));
+    if(params->scsfw_magic == 0x57464353) {
+        dprintf("sclite magic found\n");
+        return;
+    }
+    // supercard rumble
+    __aeabi_memcpy4(params, (void*)&GBA_BUS_U8[0xc0 + 4 + 0x40000], sizeof(SCSFW_PARAMETERS));
+    // account for the values being offsetted
+    params->miniboot_arm7 += 0x40000;
+    params->miniboot_arm9 += 0x40000;
+    params->nds_rom += 0x40000;
+    params->sc_lite_dldi += 0x40000;
+    params->scsd_dldi += 0x40000;
+    params->sccf_dldi += 0x40000;
 }
 
 #define SECURE_AREA_FROM_FW (*((volatile uint32_t*)0x2000000))
@@ -90,46 +90,46 @@ void findSCSFWParameters(SCSFW_PARAMETERS* params) {
 #define CHIPID (*((volatile uint32_t*)0x027FF800))
 
 typedef struct SUPERCARD_RAM_DATA {
-	uint8_t magicString[8];
-	uint8_t header[0x200];
-	uint8_t secure_area[0x4000];
-	uint32_t chipid;
+    uint8_t magicString[8];
+    uint8_t header[0x200];
+    uint8_t secure_area[0x4000];
+    uint32_t chipid;
 } SUPERCARD_RAM_DATA;
 
 static void backupDecryptedHeaderAndSecureAreaAndHeaderToSCRam(void) {
-	uint8_t magicString[8] = {'S', 'C', 'S', 'F', 'W', 0, 0, 0};
-	uint8_t magicString2[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-	volatile uint32_t* secure_area_ptr = 0;
-	uint8_t* magicStringPtr = magicString;
-	SUPERCARD_RAM_DATA* ram_data = ((SUPERCARD_RAM_DATA*)GBA_BUS);
-	
-	if(SECURE_AREA_FROM_FW != 0) {
-		secure_area_ptr = &SECURE_AREA_FROM_FW;
-	} else if(SECURE_AREA_FROM_FW_2 != 0) {
-		secure_area_ptr = &SECURE_AREA_FROM_FW_2;
-	}
-	if(secure_area_ptr == 0) {
-		// this juggling is needed because otherwise either the supercard itself
-		// or gcc simply made a single byte write not work
-		dprintf("No secure area found\n");
-		secure_area_ptr = &SECURE_AREA_FROM_FW;
-		magicStringPtr = magicString2;
-	} else {
-		dprintf("Fround secure area at address: 0x%X\n", (intptr_t)secure_area_ptr);
-		char str_buff[13];
-		__aeabi_memcpy(str_buff, NDS_HEADER->game_title, 12);
-		str_buff[13] = 0;
-		dprintf("Loaded game is: %s\n", str_buff);
-		__aeabi_memcpy(str_buff, NDS_HEADER->game_code, 4);
-		str_buff[5] = 0;
-		dprintf("Loaded game code is: %s\n", str_buff);
-	}
-	sc_change_mode(0x5);
-	__aeabi_memcpy(ram_data->magicString, magicStringPtr, 8);
-	__aeabi_memcpy4(ram_data->header, NDS_HEADER, 0x200);
-	__aeabi_memcpy4(ram_data->secure_area, secure_area_ptr, 0x4000);
-	ram_data->chipid = CHIPID;
-	sc_change_mode(en_write);
+    uint8_t magicString[8] = {'S', 'C', 'S', 'F', 'W', 0, 0, 0};
+    uint8_t magicString2[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    volatile uint32_t* secure_area_ptr = 0;
+    uint8_t* magicStringPtr = magicString;
+    SUPERCARD_RAM_DATA* ram_data = ((SUPERCARD_RAM_DATA*)GBA_BUS);
+    
+    if(SECURE_AREA_FROM_FW != 0) {
+        secure_area_ptr = &SECURE_AREA_FROM_FW;
+    } else if(SECURE_AREA_FROM_FW_2 != 0) {
+        secure_area_ptr = &SECURE_AREA_FROM_FW_2;
+    }
+    if(secure_area_ptr == 0) {
+        // this juggling is needed because otherwise either the supercard itself
+        // or gcc simply made a single byte write not work
+        dprintf("No secure area found\n");
+        secure_area_ptr = &SECURE_AREA_FROM_FW;
+        magicStringPtr = magicString2;
+    } else {
+        dprintf("Fround secure area at address: 0x%X\n", (intptr_t)secure_area_ptr);
+        char str_buff[13];
+        __aeabi_memcpy(str_buff, NDS_HEADER->game_title, 12);
+        str_buff[13] = 0;
+        dprintf("Loaded game is: %s\n", str_buff);
+        __aeabi_memcpy(str_buff, NDS_HEADER->game_code, 4);
+        str_buff[5] = 0;
+        dprintf("Loaded game code is: %s\n", str_buff);
+    }
+    sc_change_mode(0x5);
+    __aeabi_memcpy(ram_data->magicString, magicStringPtr, 8);
+    __aeabi_memcpy4(ram_data->header, NDS_HEADER, 0x200);
+    __aeabi_memcpy4(ram_data->secure_area, secure_area_ptr, 0x4000);
+    ram_data->chipid = CHIPID;
+    sc_change_mode(en_write);
 }
 
 int main(void) {
@@ -156,9 +156,9 @@ int main(void) {
     debugEnabled = ((~REG_KEYINPUT) & (KEY_L | KEY_R)) == (KEY_L | KEY_R);
 #endif
 #endif
-	sysSetCartOwner(true);
-	SUPERCARD_TYPE type = detect_supercard_type();
-	findSCSFWParameters(&parameters);
+    sysSetCartOwner(true);
+    SUPERCARD_TYPE type = detect_supercard_type();
+    findSCSFWParameters(&parameters);
     dprintf("parameters.miniboot_arm9_size: 0x%X\n", parameters.miniboot_arm9_size);
     dprintf("parameters.miniboot_arm9: 0x%X\n", parameters.miniboot_arm9);
     dprintf("parameters.miniboot_arm7_size: 0x%X\n", parameters.miniboot_arm7_size);
@@ -178,7 +178,8 @@ int main(void) {
     }
     dprintf(" OK\n");
 
-	backupDecryptedHeaderAndSecureAreaAndHeaderToSCRam();
+    backupDecryptedHeaderAndSecureAreaAndHeaderToSCRam();
+    sc_flash_rw_enable(type);
 
     // Always a bootstub in memory.
     if (true) {
@@ -202,28 +203,27 @@ int main(void) {
     // Create a copy of the DLDI driver in VRAM before initializing it.
     // We'll make use of this copy for patching the ARM9 binary later.
     dprintf("reading dldi... ");
-	sc_flash_rw_enable(type);
-	if(type & SC_LITE) {
-		dprintf("for sclite...\n");
-		__aeabi_memcpy4(DLDI_BACKUP, (void*)&GBA_BUS_U8[parameters.sc_lite_dldi], parameters.sc_lite_dldi_size);
-	} else if (type == SC_CF) {
-		dprintf("for sccf...\n");
-		__aeabi_memcpy4(DLDI_BACKUP, (void*)&GBA_BUS_U8[parameters.sccf_dldi], parameters.sccf_dldi_size);
-	} else {
-		dprintf("for scsd...\n");
-		__aeabi_memcpy4(DLDI_BACKUP, (void*)&GBA_BUS_U8[parameters.scsd_dldi], parameters.scsd_dldi_size);
-	}
+    if(type & SC_LITE) {
+        dprintf("for sclite...\n");
+        __aeabi_memcpy4(DLDI_BACKUP, (void*)&GBA_BUS_U8[parameters.sc_lite_dldi], parameters.sc_lite_dldi_size);
+    } else if (type == SC_CF) {
+        dprintf("for sccf...\n");
+        __aeabi_memcpy4(DLDI_BACKUP, (void*)&GBA_BUS_U8[parameters.sccf_dldi], parameters.sccf_dldi_size);
+    } else {
+        dprintf("for scsd...\n");
+        __aeabi_memcpy4(DLDI_BACKUP, (void*)&GBA_BUS_U8[parameters.scsd_dldi], parameters.scsd_dldi_size);
+    }
 
     // Mount the filesystem. Try to open BOOT.NDS.
     dprintf("Mounting FAT filesystem... ");
     checkErrorFatFs("Could not mount FAT filesystem", f_mount(&fs, "", 1));
     dprintf("OK\n");
     checkErrorFatFs("Could not find BOOT.NDS", f_open(&fp, executable_path, FA_READ));
-    dprintf("BOOT.NDS found.\n"	);
+    dprintf("BOOT.NDS found.\n");
 
     // Read the .nds file header.
     checkErrorFatFs("Could not read BOOT.NDS", f_read(&fp, NDS_HEADER, sizeof(nds_header_t), &bytes_read));
-	readNds(NDS_HEADER, 0, sizeof(nds_header_t));
+    readNds(NDS_HEADER, 0, sizeof(nds_header_t));
 
     bool waiting_arm7 = false;
     // Load the ARM7 binary.
@@ -241,7 +241,7 @@ int main(void) {
 
         checkErrorFatFs("Could not read BOOT.NDS", f_lseek(&fp, NDS_HEADER->arm7_offset));
         checkErrorFatFs("Could not read BOOT.NDS", f_read(&fp, (void*) (in_arm7_ram ? 0x2000000 : NDS_HEADER->arm7_start), NDS_HEADER->arm7_size, &bytes_read));
-		readNds((void*) (in_arm7_ram ? 0x2000000 : NDS_HEADER->arm7_start), NDS_HEADER->arm7_offset, NDS_HEADER->arm7_size);
+        readNds((void*) (in_arm7_ram ? 0x2000000 : NDS_HEADER->arm7_start), NDS_HEADER->arm7_offset, NDS_HEADER->arm7_size);
 
         // If the ARM7 binary has to be relocated to ARM7 RAM, the ARM7 CPU
         // has to relocate it from main memory.
@@ -271,7 +271,7 @@ int main(void) {
             waiting_arm7 = false;
         }
         checkErrorFatFs("Could not read BOOT.NDS", f_read(&fp, (void*) NDS_HEADER->arm9_start, NDS_HEADER->arm9_size, &bytes_read));
-		readNds((void*)NDS_HEADER->arm9_start, NDS_HEADER->arm9_offset, NDS_HEADER->arm9_size);
+        readNds((void*)NDS_HEADER->arm9_start, NDS_HEADER->arm9_offset, NDS_HEADER->arm9_size);
 
         // Try to apply the DLDI driver patch.
         result = dldi_patch_relocate((void*) NDS_HEADER->arm9_start, NDS_HEADER->arm9_size, DLDI_BACKUP);
